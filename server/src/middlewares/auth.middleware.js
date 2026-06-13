be59eba4a3dae80331ae44b5a0af960ce622b819
Baptista Valeta <gabryelvaleta@gmail.com>
2026-06-13 02:00:25 +0100

    Middleware para autenticação dos usuários

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import { tokenVerify } from "../config/jwt.js";
+import userModel from "../models/user.model.js";
+
+export const authMiddeware = async (req, res, next) => {
+    try {
+        const header = req.headers['authorization'];
+
+        console.log({header: header});    
+
+        if(!header) {
+            return res.status(400).json({message: "Token não fornecido"});
+        };
+        
+        const token = header
+
+        const decode = tokenVerify(token);
+        console.log(decode);
+
+        if(!decode) {
+            return res.status(400).json({message: "Token Inválido", error: err.message});
+        };
+
+        const user = await userModel.findById({_id: decode._id});
+
+        req.user = user;
+
+        next();
+    }catch (err) {
+        res.status(401).json({message: "Token Inválido"});
+    };
+}; 
