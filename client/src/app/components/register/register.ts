46e5c3b7e0258c2bbc28ddaf853e1d613b4e6ebc
Baptista Valeta <gabryelvaleta@gmail.com>
2026-07-11 02:40:57 +0100

    Alerta de erro ao cadastrar usuário

--- client/src/app/components/register/register.ts
+++ client/src/app/components/register/register.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from "@angular/router";
+
+import { ToastrService } from 'ngx-toastr';
+
 import { AuthService } from '../../core/services/auth';
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -15,7 +18,8 @@ export class Register {
   registerForm: FormGroup = new FormGroup({
     name: new FormControl('', [
       Validators.required
-    ]),
+    ]),    // if(this.password || this.email) return this.login.reset();
+
     email: new FormControl('', [
       Validators.required,
       Validators.email
@@ -27,14 +31,44 @@ export class Register {
   });
   
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService, 
+    private toast: ToastrService,
+    private routeService: Router
+  ) {}
 
-  onSubmitRegister(): void {
-    const registerData = this.registerForm.value
-    this.authService.postRegisterUser(registerData);
+  onSubmitRegister() {
+    const registerData = this.registerForm.value;
+    this.authService.postRegisterUser(registerData).subscribe({
+      next: (res) => {
+        const response: any = res;
+        const username = response.user.name;
 
-    console.log("Dados do usuário para o login",this.registerForm.value)
+        console.log('Usuário', username);
+        console.log('Resposta da api', response);    
+        
+        this.toast.success(`Cadastrado`, 'Sucesso');
+        this.registerForm.reset();
+      },
+      error: (err) => {
+        switch(err.status) {
+          case 400:
+            console.error('Email já cadastrado!', err);
+            this.toast.error('Email já cadastrado!');
+            break;
+          case 500: 
+            this.toast.error('Ocorreu um erro ao registrar', 'Erro');
+            console.error('Erro interno do servidor', err);
+            break;
+          default: 
+            this.toast.error('Servidor fora de serviço', 'Erro');
+            console.error('Servidor offline', err);
+        }
+      }
+    });
+    
+    console.log("Dados do usuário para o cadastro",this.registerForm.value);
 
     // this.registerForm.reset();
-  }
+  };
 }
