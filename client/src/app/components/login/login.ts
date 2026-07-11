37a01a6e7cfbb53c87018dce2538137f190042ce
Baptista Valeta <gabryelvaleta@gmail.com>
2026-07-11 02:39:52 +0100

    Alertas de erro no template

--- client/src/app/components/login/login.ts
+++ client/src/app/components/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ɵInternalFormsSharedModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 
 
 import { AuthService } from '../../core/services/auth';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -13,6 +14,7 @@ import { AuthService } from '../../core/services/auth';
 })
 export class Login {
   validatorPasswordCaracter: number = 4;
+  token: string = '';
 
   login = new FormGroup({
     email: new FormControl('', [
@@ -27,22 +29,61 @@ export class Login {
 
   get email() {
     return this.login.get("email");
-  } ;
+  };
 
   get password() {
     return this.login.get("password");
-  }
-
-
-  constructor(private authService: AuthService) {}
-  
-  onSubmitLogin() {
-    const dados = this.login.value;
-    this.authService.postLoginUser(dados);
-
-    console.log(this.email?.value);
-    console.log(this.password?.value);
-
-    if(this.password) return this.login.reset();
   };
-}
+
+  constructor(
+    private authService: AuthService, 
+    private routeService: Router, 
+    private toast: ToastrService
+  ) {};
+  
+  ngOnInit() {
+    // this.authService.isLoggedUser();
+    this.toast.success('SUCESSO');
+  };
+
+  onSubmitLogin(dados: any) {
+    console.log('Dados do login Valetilson:', dados);
+    this.authService.postLoginUser(dados).subscribe({
+      next: (res) => {
+        console.log('retorno da api',res);
+        const data: any = res;
+        this.token = data.token;
+
+        // Armazenar token no localStorage
+        this.authService.setToken(this.token);
+
+        console.log('Token:', this.token);
+
+        setTimeout(() => {
+          console.log('Navegando para dashboard!');
+          this.routeService.navigate(['dashboard']);
+        }, 1500);
+
+      },
+      error: (err) => {
+
+        if ((err.status === 400) || (err.status === 401)) {
+          this.toast.error('Email ou senha inválida!');
+          console.error('Credenciais Inválidas', err);
+          return;
+        }else if(err.status === 0){
+          this.toast.error('Servidor Fora de Serviço!', 'Erro');
+          console.error('Servidor Offline ou fora de Serviço', err);
+          return;
+        };
+        
+        this.toast.error('Ocorreu um erro ao fazer login!', 'Erro');
+        console.error("Erro Interno do Servidor:", err);
+      }
+    });
+
+    // console.log('login response', loginResponse);
+
+    if(this.password || this.email) return this.login.reset();
+  };
+};
