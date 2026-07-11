d4ccea35568c4c3427ed71ef9f016fe509cb99f8
Baptista Valeta <gabryelvaleta@gmail.com>
2026-07-11 02:42:20 +0100

    Rota no servidor para gestão de usuários(delete)

--- server/src/routes/admin.route.js
+++ server/src/routes/admin.route.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
+
 import { authMiddeware } from "../middlewares/auth.middleware.js";
-import { getAllUsers } from "../controllers/admin.controller.js";
+import { getAllUsers, deleteUser } from "../controllers/admin.controller.js";
 
 const router = Router();
 
 router.route("/api/auth/admin")
     .all(authMiddeware)
     .get(getAllUsers)
+
+router.delete('/api/auth/admin/remove/:id', authMiddeware, deleteUser)
 
 
 export default router;
