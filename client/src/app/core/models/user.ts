cd7c0dbcf4d5b74a73251ec1f51bc134feb2fac6
Baptista Valeta <gabryelvaleta@gmail.com>
2026-07-14 22:37:45 +0100

    Interface que referencia a entidade user da BD

---
+++ client/src/app/core/models/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+  user: {
+    _id: string,
+    name: string,
+    email: string,
+    password: string,
+    saldo: number,
+    totalEntrado: number,
+    totalSaido: number,
+    isActive: boolean,
+    role: string,
+    createdAt: string,
+    updatedAt: string
+  };
+}
