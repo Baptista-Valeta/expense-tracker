ecdb817ce1b4be0ac33ef53471efcca5632d5b31
Baptista Valeta <gabryelvaleta@gmail.com>
2026-07-18 23:43:28 +0100

    Rota para fornecer dados para o gráfico de pizza

--- server/src/routes/report.route.js
+++ server/src/routes/report.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { authMiddeware } from "../middlewares/auth.middleware.js";
-import { report } from "../controllers/report.controller.js";
+import { getChartData, report } from "../controllers/report.controller.js";
 
 const router = Router();
 
 router.get("/api/reports/summary", authMiddeware, report);
+router.get("/api/reports/chart-data", authMiddeware, getChartData)
 
 export default router;
