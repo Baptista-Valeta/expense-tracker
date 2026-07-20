82969c08e8b1ce8bb2ba9711bb1e72218cd9269d
Baptista Valeta <gabryelvaleta@gmail.com>
2026-07-20 23:45:14 +0100

    Rota para o dados do gráfico de gastos mensais

--- server/src/routes/report.route.js
+++ server/src/routes/report.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import { authMiddeware } from "../middlewares/auth.middleware.js";
-import { getChartData, report } from "../controllers/report.controller.js";
+import { getChartDataCategory, getChartDataMounthly, report } from "../controllers/report.controller.js";
 
 const router = Router();
 
 router.get("/api/reports/summary", authMiddeware, report);
-router.get("/api/reports/chart-data", authMiddeware, getChartData)
+router.get("/api/reports/chart-data-category", authMiddeware, getChartDataCategory);
+router.get("/api/reports/chart-data-mounthly", authMiddeware, getChartDataMounthly);
 
 export default router;
