9cd65fcc8f7b13bf58e7dba064a9feee77f3b16d
Baptista Valeta <gabryelvaleta@gmail.com>
2026-08-13 23:46:41 +0100

    Service para tratar as categorias

---
+++ client/src/app/core/services/category.ts
@@ -0,0 +1,62 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
+
+import { Categories, AllCategories } from '../../core/models/category';
+import { AuthService } from './auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  apiUrl = inject(AuthService).apiUrl;
+  categories = signal<AllCategories['categories']|null>(null);
+
+  constructor(private http: HttpClient) {};
+
+  createCategory(payload: any) {
+    return this.http.post<Categories>(this.apiUrl+'categories', payload).pipe(
+      map(category => {
+        console.log('Create Category', category.categories);
+        return category.categories;
+      }),
+      catchError(error => {
+        if(error.status === 400) {
+          console.error('Erro ao criar categoria', error);
+          return throwError(() => 'Campo Inválido');
+        };
+        return throwError(() => error.message);
+      })
+    );
+  };
+
+  getAllCategory() {
+    return this.http.get<AllCategories>(this.apiUrl+'categories').pipe(
+      map(categories => {
+        console.log('Todas as categorias', categories.categories);
+        return categories.categories;
+      }),
+      catchError(error => {
+        if(error.status === 404) {
+          return throwError(() => 'Nenhuma categoria encontrada');
+        };
+        console.error('Erro ao buscar categoria:', error)
+        return throwError(() => error);
+      })
+    );
+  };
+
+  deleteCategory(id: string) {
+    return this.http.delete(this.apiUrl+'categories/'+id).pipe(
+      map(res => {
+        console.log('[DELETE CATEGORY]',res);
+      }),
+      catchError((error) => {
+        if(error.status === 400) {
+          return throwError(() => error.error);          
+        }
+        return throwError(() => error);
+      })
+    )
+  }
+}
