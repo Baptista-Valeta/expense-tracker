cd26b6868096024c1d4db27dc59390d7f498772e
Baptista Valeta <gabryelvaleta@gmail.com>
2026-08-31 23:12:54 +0100

    Funcionalidade para atualizar perfil(email e nome) e password

--- client/src/app/components/profile/profile.ts
+++ client/src/app/components/profile/profile.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
+import { tap } from 'rxjs';
+
+import * as bootstrap from 'bootstrap';
+import { ToastrService } from 'ngx-toastr';
+
 import { AuthService } from '../../core/services/auth';
-import { FormControl, FormGroup, ReactiveFormsModule, ɵInternalFormsSharedModule } from '@angular/forms';
+import { Login } from '../login/login';
 
 @Component({
   selector: 'app-profile',
@@ -10,22 +16,109 @@ import { FormControl, FormGroup, ReactiveFormsModule, ɵInternalFormsSharedModul
 })
 export class Profile {
   editProfile: FormGroup = new FormGroup({
-    name: new FormControl('', []),
-    email: new FormControl('', [])
+    name: new FormControl('', [
+      Validators.required,
+    ]),
+    email: new FormControl('', [
+      Validators.email,
+      Validators.required
+    ]),
   });
 
-  constructor(protected authService: AuthService) {};
+  validatorPasswordCaracter = inject(Login).validatorPasswordCaracter;
+  ChangePasswordForms: FormGroup = new FormGroup({
+    currentPassword: new FormControl('', [
+      Validators.minLength(this.validatorPasswordCaracter),
+      Validators.required
+    ]),
+    confirm: new FormControl('', [
+      Validators.required
+    ]),
+    newPassword: new FormControl('', [
+      Validators.minLength(this.validatorPasswordCaracter),
+      Validators.required
+    ])
+  });
+  
+  name () {
+    return this.editProfile.get('name');
+  };
 
+  email () {
+    return this.editProfile.get('email');
+  };
+
+  current () {
+    return this.ChangePasswordForms.get('currentPassword');
+  };
+
+  confirm () {
+    return this.ChangePasswordForms.get('confirm');
+  };
+
+  newPassword () {
+    return this.ChangePasswordForms.get('newPassword');
+  };
+  
+  constructor(protected authService: AuthService, private toastr: ToastrService) {};
+  
   ngOnInit() {
     this.authService.getDataUser().subscribe(data => {
       this.editProfile.setValue({
         name: data.name,
-        email: data.email
+        email: data.email,
       });
-    })
+    });
   };
 
-  openForm(name: string, email: string) {
-    alert(`${name}, ${email}`);
+  UpdateProfile() {
+    const name = this.name();
+    const email = this.email();
+    
+    if(name?.invalid || email?.invalid) {
+      alert('Nome ou email inválido');
+      return;
+    };
+
+    this.authService.putProfile({name: name?.value, email: email?.value}).subscribe(data => {
+      this.toastr.success('Perfil atualizado');
+    });
+  };
+  
+  changePassword() {
+    this.ChangePasswordForms.markAllAsTouched();
+
+    console.log(this.ChangePasswordForms.value, this.email()?.value);
+    if(this.ChangePasswordForms.invalid) {
+      return;
+    };
+
+    const payload = {email: this.email()?.value, password: this.current()?.value};
+    this.authService.postLoginUser(payload).subscribe({
+      next: data => {
+        // current == ok
+        console.log('TOKEN', data);
+        if(this.current()?.value === this.confirm()?.value) {
+          this.authService.putProfile({password: this.newPassword()?.value}).subscribe(response => {
+            console.log('UPDATE PASSWORD', response);
+            this.toastr.success('Palavra-passe alterada');
+          });
+        }else {
+          this.toastr.error('A confirmação está incorreta');
+        };
+      },
+      error: err => {
+        this.toastr.error('A palavra-passe atual está incorreta');
+      }
+    });
+  };
+
+  openForm() {
+    const modal = new bootstrap.Modal(document.getElementById('exampleModalToggle') as HTMLElement);
+    modal.show();
+  };
+
+  resetForm() {
+    this.ChangePasswordForms.reset();
   }
 }
