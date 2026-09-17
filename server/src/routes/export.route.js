db4426fbd2fefc622e6befd06ae5f5825251fb56
Baptista Valeta <gabryelvaleta@gmail.com>
2026-09-17 23:58:08 +0100

    Arquivo de rota para exportar os dados de transação.

---
+++ server/src/routes/export.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import { authMiddeware } from "../middlewares/auth.middleware.js";
+import { ExportData } from '../controllers/export.controller.js'
+
+const router = Router();
+
+router.get('/api/reports/export', ExportData);
+
+export default router;
