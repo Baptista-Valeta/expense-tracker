592b247937237d552344463874acca877dd2d2a0
Baptista Valeta <gabryelvaleta@gmail.com>
2026-09-18 23:53:02 +0100

    Apresentando a categoria correspondente em cada transação através do seu objectId.

--- server/src/services/export.service.js
+++ server/src/services/export.service.js
@@ -1,14 +1,31 @@
+import categoryModel from "../models/category.model.js";
 import transactionModel from "../models/transaction.model.js";
 
 export const exportTransactions = async (user_id) => {
     try{
-        const transactions = await transactionModel
-            .find({user: user_id})
+        let transactions = await transactionModel
+            .find({type: 'income'})
             .sort({date: -1})
-            .lean(); // Retorna documento js simples ao invés de documentos mongoose
-        
+            .lean(); // Retorna documento js simples ao invés de documentos mongoose completos
+                
+        let category;
+        let transactions_formats = [];
+        for(const transaction of transactions) {
+            category = await categoryModel.findById(transaction.category);    
+
+            transactions_formats.push({
+                description: transaction.description,
+                amount: transaction.amount,
+                type: transaction.type,
+                date: transaction.date,
+                category: category.name
+            });
+        };
+
+        console.log('transactions', transactions);
+
         const headers = ['Descrição', 'Valor', 'Tipo', 'Data', 'Categoria'];
-        const rows = transactions.map(transaction => [
+        const rows = transactions_formats.map(transaction => [
             transaction.description,
             transaction.amount,
             transaction.type,
@@ -16,17 +33,16 @@ export const exportTransactions = async (user_id) => {
             transaction.category
         ]);
 
-        console.log('rows', rows);
+        console.log('rows', rows.length);
 
         const csv = [headers, ...rows]
-            .map(row => row.map(escapeCsvValue()).join(','))
+            .map(row => row.map(escapeCsvValue).join(','))
             .join('\n');
 
-        console.log('csv', csv)
+        // console.log('csv', csv)
         return '\ufeff' + csv;
     } catch (error) {
-        console.log('Erro ao criar csv.');
-        throw Error('Erro ao criar csv.');
+        console.log('Erro ao criar csv.', error.message);
     };
 };
 
